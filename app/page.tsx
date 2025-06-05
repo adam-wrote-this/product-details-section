04ce7ffbd898e7e423c3a345eceb0672f4dadb85
Adam · Li <775837740@qq.com>
2025-06-05 23:47:54 +0800

    fix: Fix responsive layout issues on desktop devices

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { data } from './data'
 
 export default function Home() {
   return (
-    <div className='w-[375px] md:w-[768px] xl:w-full flex justify-center items-center gap-2 bg-gradient-to-b from-gray-50 to-[#d2d6db]'>
+    <div className='w-[375px] md:w-full xl:w-full flex justify-center items-center gap-2 bg-gradient-to-b from-gray-50 to-[#d2d6db]'>
       <Product>
         <Product.Images images={data.images} />
         <Product.Details
